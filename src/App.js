52d78f575cd5d1919abbb00f8251489aac39725e
ryan <ryanqueiroz2012@gmail.com>
2023-06-21 22:12:31 -0300

    Primeiras alterações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Illustration from './images/illustration-hero.svg'
+import IconMusic from './images/icon-music.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={Illustration} alt="#" />
+        <h2>Order Summary</h2>
+        <p>You can now listen to millions of songs, audiobooks, and podcasts on any 
+        device anywhere you like!</p>
+        <div className="plans">
+          <img src={IconMusic} alt="#" />
+          <div className="annualPlan">
+            <h4>Annual Plan</h4>
+            <p>$59.99/year</p>
+          </div>
+          <a href="#">Change</a>
+        </div>
+        <button>Proceed to Payment</button>
+        <button>Cancel Order</button>
+      </div>
     </div>
   );
 }
